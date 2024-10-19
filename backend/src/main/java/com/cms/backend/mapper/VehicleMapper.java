f357d782d74797903d9dca20e9ff82c329cb6b48
Seamher <947211502@qq.com>
2024-10-19 23:58:49 +0800

    构建后端

---
+++ backend/src/main/java/com/cms/backend/mapper/VehicleMapper.java
@@ -0,0 +1,8 @@
+package com.cms.backend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cms.backend.pojo.Vehicle;
+
+public interface VehicleMapper extends BaseMapper<Vehicle> {
+
+}
---
+++ backend/src/main/java/com/cms/backend/service/Impl/FrameServiceImpl.java
@@ -0,0 +1,10 @@
+package com.cms.backend.service.Impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.cms.backend.mapper.FrameMapper;
+import com.cms.backend.pojo.Frame;
+import com.cms.backend.service.FrameService;
+
+public class FrameServiceImpl extends ServiceImpl<FrameMapper, Frame> implements FrameService {
+
+}
